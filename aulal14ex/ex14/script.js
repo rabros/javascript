cd64540e0dbb6c547c25e454ee7ba342c8e26484
João Barros <105664722+rabros@users.noreply.github.com>
2023-09-05 14:37:51 -0300

    Ajustando exercício Contagem

--- aulal14ex/ex14/script.js
+++ aulal14ex/ex14/script.js
@@ -1,12 +1,31 @@
 function contar(){
-    var tinicio = document.getElementById('txtinicio')
-    var tfim = document.getElementById('txtfim')
-    var tpasso = document.getElementById('txtpasso')
-    var inicio = Number(tinicio.value)
-    var fim = Number(tfim.value)
-    var passo = Number(tpasso.value)
-    var res = document.querySelector('div#res')
-    for(var c = inicio; c <= fim; c += passo){
-        res.innerHTML = `${c}`
+    var ini = document.getElementById('txtinicio')
+    var fim = document.getElementById('txtfim')
+    var passo = document.getElementById('txtpasso')
+    var res = document.getElementById('res')
+    if(ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0){
+        alert("[ERRO] Faltam dados!")
+        res.innerHTML = 'Impossivel contar!'
+    } else{
+        res.innerHTML = 'Contanto: <br>'
+        var i = Number(ini.value)
+        var f = Number(fim.value)
+        var p = Number(passo.value)
+        if(p<=0){
+            alert('Passo invalido! Considerando Passo = 1')
+            p = 1
+        }
+        if(i < f){
+            // Contagem crescente
+            for(let c = i; c <= f; c += p){
+                res.innerHTML += ` ${c} \u{1F449}`
+            }
+        } else {
+            // Contagem regressiva
+            for(var c = i; c >= f; c -= p){
+                res.innerHTML += ` ${c} \u{1F449}`
+            }
+        }
+    res.innerHTML += `\u{1F3C1}`
     }
 }
